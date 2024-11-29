45f9f7f28643e3e6cf13625479b68a259bf7025d
OMKAR PRASAD BARAI <omkarprasadbarai7597@gmail.com>
2024-11-29 10:30:00 +0530

    adding create palindrome number

---
+++ nov-29/palindrome.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str, rev;
+    cout << "Enter a string: ";
+    cin >> str;
+    rev = string(str.rbegin(), str.rend());
+    if (str == rev)
+        cout << str << " is a palindrome." << endl;
+    else
+        cout << str << " is not a palindrome." << endl;
+    return 0;
+}
